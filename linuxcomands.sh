6e58f76adb4239ea0d96a0c10f6aaf50a9fffd9a
sreevarshitha1234 <srivarshithaias@gmail.com>
2024-07-29 17:24:19 +0530

    .

---
+++ linuxcomands.sh
@@ -0,0 +1,15 @@
+ip address :
+1. ip a 
+2. ip addr
+3. ip address
+4. if config (works only in runlevel 5)
+run levels :
+total runlevels = 0+6
+1. run level 0 
+   init 0 ---> 
+2. run level 1
+3. run level 2
+4. run level 3
+5. run level 4
+6. run level 5
+7. run level 6
